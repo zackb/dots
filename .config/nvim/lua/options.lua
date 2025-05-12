44aca9bd7ff777309c255cae174783215c910dd1
Zack Bartel <zack@bartel.com>
2025-05-12 14:43:59 -0700

    update vimrc

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,1 +1,13 @@
 require "nvchad.options"
+
+vim.opt.syntax = "on"
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.number = true
+vim.opt.foldmethod = "marker"
+vim.opt.autoindent = true
+vim.opt.incsearch = true
+vim.opt.wildmenu = true
+vim.opt.ruler = true
+
