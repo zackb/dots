596cd5d66348b0e54faad3fbaa464aac1bd057e0
Zack Bartel <zack@bartel.com>
2025-05-12 19:18:10 -0700

    build+run

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -4,3 +4,7 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+
+map("n", "<leader>r", function()
+  vim.cmd "!cmake --preset debug build && ./build/debug/meconium"
+end, { desc = "Build and run C++ app" })
