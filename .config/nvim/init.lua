1bfa8615de0ce44a95b3e920639e1e089ebe7e7c
Zack Bartel <zack@bartel.com>
2025-05-14 22:43:48 -0700

    grumble

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,16 +1,17 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system {
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    }
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("vim-options")
-require("mappings")
-require("lazy").setup("plugins")
+vim.keymap.set("n", "*", "*", { noremap = true })
+require "vim-options"
+require "mappings"
+require("lazy").setup "plugins"
