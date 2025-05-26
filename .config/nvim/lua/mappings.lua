575abf2edb4bd19cbfcc5eccc37eecbbf1c441fa
Zack Bartel <zack@bartel.com>
2025-05-26 14:09:17 -0700

    vim oldfile

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,3 +1,7 @@
 vim.keymap.set("n", "<leader>r", function()
     vim.cmd "!cmake --build --preset debug && ./build/debug/meconium"
 end, { desc = "Build and run C++ app" })
+
+vim.keymap.set("n", "<leader>fr", function()
+    vim.cmd ":browse oldfiles"
+end, { desc = "Browse old files" })
