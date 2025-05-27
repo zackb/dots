f2602edc1439204ffbcd2f16e00bf19efb225208
Zack Bartel <zack@bartel.com>
2025-05-26 17:28:25 -0700

    leader-d = telescope diag

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -9,3 +9,5 @@ end, { desc = "Browse old files" })
 vim.keymap.set("n", "<leader>e", function()
     vim.cmd ":Neotree reveal"
 end, { desc = "Reveal current file" })
+
+vim.keymap.set("n", "<leader>q", "<cmd>Telescope diagnostics bufnr=0<cr>", { desc = "Show diagnostics for buffer" })
