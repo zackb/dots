d54685ba5d5e01d83078b0023720472892ff83b2
Zack Bartel <zack@bartel.com>
2025-05-30 19:42:25 -0700

    fmt

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,46 +1,46 @@
 return {
-    { 
-        'saghen/blink.cmp',
-            event = 'VimEnter',
-            version = '1.*',
-            dependencies = {
-                -- Snippet Engine
-                {
-                    'L3MON4D3/LuaSnip',
-                    version = '2.*',
-                    dependencies = {
-                        {
-                            'rafamadriz/friendly-snippets',
-                            config = function()
-                                require('luasnip.loaders.from_vscode').lazy_load()
-                                end,
-                        },
-                    },
-                    opts = {},
-                },
-                'folke/lazydev.nvim',
-            },
-            opts = {
-                keymap = {
-                    preset = 'default',
-                },
-                appearance = {
-                    nerd_font_variant = 'normal',
-                },
-
-                completion = {
-                    documentation = { auto_show = false, auto_show_delay_ms = 500 },
-                },
-                sources = {
-                    default = { 'lsp', 'path', 'snippets', 'lazydev' },
-                    providers = {
-                        lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+    {
+        "saghen/blink.cmp",
+        event = "VimEnter",
+        version = "1.*",
+        dependencies = {
+            -- Snippet Engine
+            {
+                "L3MON4D3/LuaSnip",
+                version = "2.*",
+                dependencies = {
+                    {
+                        "rafamadriz/friendly-snippets",
+                        config = function()
+                            require("luasnip.loaders.from_vscode").lazy_load()
+                        end,
                     },
                 },
-
-                snippets = { preset = 'luasnip' },
-                fuzzy = { implementation = 'lua' },
-                signature = { enabled = true },
+                opts = {},
             },
+            "folke/lazydev.nvim",
+        },
+        opts = {
+            keymap = {
+                preset = "default",
+            },
+            appearance = {
+                nerd_font_variant = "normal",
+            },
+
+            completion = {
+                documentation = { auto_show = true, auto_show_delay_ms = 500 },
+            },
+            sources = {
+                default = { "lsp", "path", "snippets", "lazydev" },
+                providers = {
+                    lazydev = { module = "lazydev.integrations.blink", score_offset = 100 },
+                },
+            },
+
+            snippets = { preset = "luasnip" },
+            fuzzy = { implementation = "lua" },
+            signature = { enabled = true },
+        },
     },
 }
