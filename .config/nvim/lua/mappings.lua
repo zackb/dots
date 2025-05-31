8da67868b7066965df70d55a854c4166604f4df5
Zack Bartel <zack@bartel.com>
2025-05-30 22:41:36 -0700

    cleanup

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -13,5 +13,13 @@ end, { desc = "Reveal current file" })
 vim.keymap.set("n", "<leader>q", "<cmd>Telescope diagnostics bufnr=0<cr>", { desc = "Show diagnostics for buffer" })
 
 -- yank to system clipboard
-vim.keymap.set('n', 'y', '"+y')
-vim.keymap.set('v', 'y', '"+y')
+vim.keymap.set("n", "y", '"+y')
+vim.keymap.set("v", "y", '"+y')
+--
+-- clear highlights on search when pressing <Esc> in normal mode
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -10,10 +10,14 @@ vim.opt.incsearch = true
 vim.opt.swapfile = false
 vim.opt.number = true
 vim.opt.autoindent = true
+vim.opt.smartindent = true
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.g.have_nerd_font = false
+
+vim.opt.termguicolors = true
+vim.opt.swapfile = false
 
 -- vim.schedule(function()
 --   vim.o.clipboard = 'unnamedplus'
@@ -21,24 +25,17 @@ vim.g.have_nerd_font = false
 
 vim.o.undofile = false
 
-vim.o.inccommand = 'split'
+vim.o.inccommand = "split"
 vim.o.cursorline = true
 vim.o.scrolloff = 10
-
--- clear highlights on search when pressing <Esc> in normal mode
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.opt.colorcolumn = "80"
 
 vim.o.showtabline = 1
 vim.o.tabline = "%!v:lua.TabLine()"
 
 function _G.TabLine()
     local s = ""
-    for i = 1, vim.fn.tabpagenr("$") do
+    for i = 1, vim.fn.tabpagenr "$" do
         local winnr = vim.fn.tabpagewinnr(i)
         local buflist = vim.fn.tabpagebuflist(i)
         local bufnr = buflist[winnr]
@@ -54,4 +51,3 @@ function _G.TabLine()
     s = s .. "%#TabLineFill#"
     return s
 end
-
