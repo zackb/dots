21ff3e540615bb5838b4aad945aa724c21e586b8
Zack Bartel <zbartel@proofpoint.com>
2025-06-04 13:45:46 -0700

    change colorscheme

--- .config/nvim/lua/plugins/cattpuccin.lua
+++ .config/nvim/lua/plugins/cattpuccin.lua
@@ -1,15 +1,15 @@
 return {
-  {
-    "catppuccin/nvim",
-    lazy = false,
-    name = "catppuccin",
-    priority = 1000,
+    {
+        "catppuccin/nvim",
+        lazy = false,
+        name = "catppuccin",
+        priority = 1000,
 
-    config = function()
-      require("catppuccin").setup({
-        transparent_background = true,
-      })
-      vim.cmd.colorscheme "catppuccin-mocha"
-    end
-  }
+        config = function()
+            require("catppuccin").setup {
+                transparent_background = true,
+            }
+            -- vim.cmd.colorscheme "catppuccin-mocha"
+        end,
+    },
 }
