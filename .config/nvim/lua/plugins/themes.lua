fdbea609624bb8befbfcc406d3f192f642c011e7
Zack Bartel <zbartel@proofpoint.com>
2025-06-06 09:00:16 -0700

    try rose pine

---
+++ .config/nvim/lua/plugins/themes.lua
@@ -0,0 +1,35 @@
+return {
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        config = function()
+            vim.cmd "colorscheme rose-pine"
+        end,
+    },
+    {
+        "projekt0n/github-nvim-theme",
+        name = "github-theme",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("github-theme").setup {
+                -- ...
+            }
+
+            -- vim.cmd "colorscheme github_dark_default"
+        end,
+    },
+    {
+        "catppuccin/nvim",
+        lazy = false,
+        name = "catppuccin",
+        priority = 1000,
+
+        config = function()
+            require("catppuccin").setup {
+                transparent_background = true,
+            }
+            -- vim.cmd.colorscheme "catppuccin-mocha"
+        end,
+    },
+}
