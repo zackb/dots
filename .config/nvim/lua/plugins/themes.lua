1650bc82bfcd39e00fcdf8baa7b0814bb4f6c35e
Zack Bartel <zack@bartel.com>
2025-06-16 21:11:10 -0700

    disable gross italics

--- .config/nvim/lua/plugins/themes.lua
+++ .config/nvim/lua/plugins/themes.lua
@@ -3,6 +3,11 @@ return {
         "rose-pine/neovim",
         name = "rose-pine",
         config = function()
+            require("rose-pine").setup {
+                styles = {
+                    italic = false,
+                },
+            }
             vim.cmd "colorscheme rose-pine"
         end,
     },
