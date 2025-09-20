4b4f9df056143a3e476f6843adcabd03e8b13fb1
Zack Bartel <zack@bartel.com>
2025-09-20 13:53:55 -0700

    fix weather

--- .config/waybar/scripts/wttr.sh
+++ .config/waybar/scripts/wttr.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
-req=$(curl -s wttr.in?format="%t|%l+(%c%f)+%h,+%C")
+req=$(curl -s 'wttr.in?lang=gibberish&format=%25t%7C%25l%2B(%25c%25f)%2B%25h%2C%2B%25C')
 bar=$(echo $req | awk -F "|" '{print $1}')
 tooltip=$(echo $req | awk -F "|" '{print $2}')
 echo "{\"text\":\"$bar\", \"tooltip\":\"$tooltip\"}"
