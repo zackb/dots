a07f379aa8ed418fe75f33e49179f8125ce1d473
Zack Bartel <zack@bartel.com>
2025-10-31 14:26:10 -0700

    vcard and caldav sync

--- bin/get-net-creds.sh
+++ bin/get-net-creds.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 host=$1
-awk -v host="$host" '
+field=${2:-password}
+
+awk -v host="$host" -v field="$field" '
   $1 == "machine" && $2 == host {
-    getline; login = $2; 
+    getline; login = $2;
     getline; password = $2;
-    print password;
+    if (field == "username" || field == "login") {
+      print login;
+    } else {
+      print password;
+    }
     exit;
   }
 ' ~/.netrc
-
