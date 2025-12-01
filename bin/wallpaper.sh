bc636e9458c9d5aa9cb0d257f58ab94a69ac905e
Zack Bartel <zack@bartel.com>
2025-12-01 12:24:18 -0800

    update to persist wallpaper

--- bin/wallpaper.sh
+++ bin/wallpaper.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+
 wall=$(hyprwat --wallpaper ~/.local/share/wallpapers)
+
 if [ -n "$wall" ]; then
     sed -i "s|^\$image = .*|\$image = $wall|" ~/.config/hypr/hyprlock.conf
+    sed -i "s|^preload = .*|preload = $wall|" ~/.config/hypr/hyprpaper.conf
+    sed -i "s|^wallpaper =.*,.*|wallpaper = , $wall|" ~/.config/hypr/hyprpaper.conf
 fi
+
