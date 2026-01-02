0e213987743520c1e6bf60c409c76b29ed71e10b
Zack Bartel <zack@bartel.com>
2026-01-01 20:42:51 -0800

    update to new lsp config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -12,7 +12,6 @@ return {
         config = function()
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 
             vim.diagnostic.config {
                 virtual_text = true,
@@ -25,58 +24,21 @@ return {
             vim.api.nvim_create_autocmd("LspAttach", {
                 group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
                 callback = function(event)
-                    -- NOTE: Remember that Lua is a real programming language, and as such it is possible
-                    -- to define small helper and utility functions so you don't have to repeat yourself.
-                    --
-                    -- In this case, we create a function that lets us more easily define mappings specific
-                    -- for LSP related items. It sets the mode, buffer and description for us each time.
                     local map = function(keys, func, desc, mode)
                         mode = mode or "n"
                         vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
                     end
 
-                    -- Rename the variable under your cursor.
-                    --  Most Language Servers support renaming across files, etc.
                     map("grn", vim.lsp.buf.rename, "[R]e[n]ame")
-
-                    -- Execute a code action, usually your cursor needs to be on top of an error
-                    -- or a suggestion from your LSP for this to activate.
                     map("gra", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
-
-                    -- Find references for the word under your cursor.
                     map("grr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-
-                    -- Jump to the implementation of the word under your cursor.
-                    --  Useful when your language has ways of declaring types without an actual implementation.
                     map("gri", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-
-                    -- Jump to the definition of the word under your cursor.
-                    --  This is where a variable was first declared, or where a function is defined, etc.
-                    --  To jump back, press <C-t>.
                     map("grd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-
-                    -- WARN: This is not Goto Definition, this is Goto Declaration.
-                    --  For example, in C this would take you to the header.
                     map("grD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-
-                    -- Fuzzy find all the symbols in your current document.
-                    --  Symbols are things like variables, functions, types, etc.
                     map("gO", require("telescope.builtin").lsp_document_symbols, "Open Document Symbols")
-
-                    -- Fuzzy find all the symbols in your current workspace.
-                    --  Similar to document symbols, except searches over your entire project.
                     map("gW", require("telescope.builtin").lsp_dynamic_workspace_symbols, "Open Workspace Symbols")
-
-                    -- Jump to the type of the word under your cursor.
-                    --  Useful when you're not sure what type a variable is and you want to see
-                    --  the definition of its *type*, not where it was *defined*.
                     map("grt", require("telescope.builtin").lsp_type_definitions, "[G]oto [T]ype Definition")
 
-                    -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
-                    ---@param client vim.lsp.Client
-                    ---@param method vim.lsp.protocol.Method
-                    ---@param bufnr? integer some lsp support methods only in specific files
-                    ---@return boolean
                     local function client_supports_method(client, method, bufnr)
                         if vim.fn.has "nvim-0.11" == 1 then
                             return client:supports_method(method, bufnr)
@@ -85,11 +47,6 @@ return {
                         end
                     end
 
-                    -- The following two autocommands are used to highlight references of the
-                    -- word under your cursor when your cursor rests there for a little while.
-                    --    See `:help CursorHold` for information about when this is executed
-                    --
-                    -- When you move your cursor, the highlights will be cleared (the second autocommand).
                     local client = vim.lsp.get_client_by_id(event.data.client_id)
                     if
                         client
@@ -122,10 +79,6 @@ return {
                         })
                     end
 
-                    -- The following code creates a keymap to toggle inlay hints in your
-                    -- code, if the language server you are using supports them
-                    --
-                    -- This may be unwanted, since they displace some of your code
                     if
                         client
                         and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf)
@@ -140,7 +93,6 @@ return {
             local capabilities = require("blink.cmp").get_lsp_capabilities()
 
             local servers = {
-                -- TODO: clangd = {},
                 gopls = {},
                 terraformls = {},
                 ts_ls = {},
@@ -153,7 +105,6 @@ return {
                 yamlls = {},
                 vimls = {},
                 marksman = {},
-                -- java_language_server = {},
             }
 
             local ensure_installed = vim.tbl_keys(servers or {})
@@ -169,7 +120,15 @@ return {
                     function(server_name)
                         local server = servers[server_name] or {}
                         server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-                        require("lspconfig")[server_name].setup(server)
+
+                        vim.lsp.config[server_name] = vim.tbl_deep_extend("force", {
+                            capabilities = server.capabilities,
+                            cmd = server.cmd,
+                            filetypes = server.filetypes,
+                            root_dir = server.root_dir,
+                            settings = server.settings,
+                        }, server)
+                        vim.lsp.enable(server_name)
                     end,
                 },
             }
