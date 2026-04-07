25c9aae188f73f73f805f594b7251de635bb5e46
Zack Bartel <zack@bartel.com>
2026-04-06 18:32:53 -0700

    update to nvim 0.12

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,26 +1,22 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        auto_install = false,
-        ensure_installed = {
-          "bash",
-          "html",
-          "css",
-          "javascript",
-          "json",
-          "lua",
-          "cpp",
-          "c",
-          "java",
-          "go",
-        },
-        highlight = { enable = true },
-        indent = { enable = false },
-      })
-    end
-  }
+    {
+        "nvim-treesitter/nvim-treesitter",
+        branch = "main",
+        lazy = false,
+        build = ":TSUpdate",
+        config = function()
+            vim.api.nvim_create_autocmd("FileType", {
+                callback = function(args)
+                    local buf = args.buf
+                    local lang = vim.treesitter.language.get_lang(args.match)
+                    if lang and pcall(vim.treesitter.start, buf, lang) then
+                    -- Tree-sitter started successfully
+                    else
+                        -- Optionally fallback to regex syntax if tree-sitter fails
+                        -- vim.bo[buf].syntax = "ON"
+                    end
+                end,
+            })
+        end,
+    },
 }
