f8d3a9486a1e7cd8a5547c4230af95a60b24eb27
Zack Bartel <zack@bartel.com>
2026-06-02 22:38:02 -0700

    fin

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -24,7 +24,7 @@ local menu = "rofi -show drun"
 -------------------
 
 hl.on("hyprland.start", function()
-	hl.exec_cmd("waybar")
+	-- hl.exec_cmd("waybar")
 	hl.exec_cmd("swaync")
 	hl.exec_cmd("hypridle")
 	hl.exec_cmd("hyprpaper")
