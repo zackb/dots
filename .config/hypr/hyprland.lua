e226131eb91162c2e20d6c1fc9c64abea5044c24
Zack Bartel <zack@bartel.com>
2026-06-11 19:07:38 -0700

    another one bites the dust

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -31,7 +31,7 @@ hl.on("hyprland.start", function()
 	-- hl.exec_cmd("swaync")
 	-- hl.exec_cmd("hyprpaper")
 	-- hl.exec_cmd("hypridle")
-	hl.exec_cmd("systemctl --user start hyprpolkitagent")
+	-- hl.exec_cmd("systemctl --user start hyprpolkitagent")
 	hl.exec_cmd("quickshell")
 end)
 
